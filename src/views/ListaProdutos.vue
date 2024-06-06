7cb1b176ecd6348b8cbf3a9e2430d52954a34b31
Erik Delanda Zaros <127625388+Erik-Zaros@users.noreply.github.com>
2024-06-06 21:47:16 +0000

    Incluindo o axios na listagem dos produtos

--- src/views/ListaProdutos.vue
+++ src/views/ListaProdutos.vue
@@ -1,20 +1,36 @@
-<script setup>
-import CardProdutos from "../components/CardProdutos.vue";
-</script>
-
 <template>
   <div class="lista-produtos container-fluid d-flex justify-content-center align-items-center flex-column">
     <div class="row col-lg-11 col-md-12">
       <h4 class="text-center mt-3">Lista de Produtos da sua loja</h4>
-      <CardProdutos :exibirBotoes="true" /> 
-      <CardProdutos :exibirBotoes="true" />  <!-- false = exclui botões -->
-<!-- :exibirBotoes="true" faz com que os botões de edição e exclusão apareçam somente na lista de produtos. assim podendo reutilizar o mesmo componente diretamente na loja -->
+      <CardProdutos v-for="produto in produtos" :key="produto.id" :produto="produto" :exibirBotoes="true" />
     </div>
   </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue';
+import ProdutoService from "../services/ProdutoService";
+import CardProdutos from "../components/CardProdutos.vue";
+
+const produtos = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await ProdutoService.getAll();
+    produtos.value = response.data;
+  } catch (error) {
+    console.error("Erro ao recuperar produtos:", error);
+  }
+});
+</script>
+
 <style scoped>
 .lista-produtos {
   margin-top: 8vh;
+}
+
+/* Adicionando estilo para os cards aparecerem lado a lado */
+.card-personagens {
+  flex-basis: 25%; /* Define o tamanho do card em relação à largura da tela */
 }
 </style>
